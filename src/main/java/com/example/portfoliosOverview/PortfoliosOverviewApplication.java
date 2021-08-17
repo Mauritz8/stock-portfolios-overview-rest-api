57e3fdefa236dbf7ca2e4cd7b99c1d2a0c3d0e53
= <mauritz.sjodin3@gmail.com>
2021-08-17 07:11:34 +0200

    initial commit

---
+++ src/main/java/com/example/portfoliosOverview/PortfoliosOverviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfoliosOverview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliosOverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfoliosOverviewApplication.class, args);
+	}
+
+}
